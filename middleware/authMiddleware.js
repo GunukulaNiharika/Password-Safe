2678798c9ab103f671589f0dba3e955369b644fc
GunukulaNiharika <niharikagunukula2002@gmail.com>
2021-06-05 16:15:54 +0530

    update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-
 const requireAuth =  (req, res, next) => {
     const token = req.cookies.jwt; 
     // check json web token exists & is verified
